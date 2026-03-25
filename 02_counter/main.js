0fcfc9a8ca10567ed113b2c3998bc49aaf7898ab
yimchaei <babybreadin1025@gmail.com>
2026-03-26 08:56:33 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,7 +1,7 @@
 // const h1 = document.querySelector("h1");
 // const button = document.querySelector("button");
 const h1 = document.getElementById("redult")
-const plusButton = document.getElementsByClassName("plusButton")[0];
+// const plusButton = document.getElementsByClassName("plusButton")[0];
 
 let count = 0;
 
@@ -10,7 +10,11 @@ let count = 0;
 //     h1.textContent = count;
 // };
 
-button.onclick= () => {
+// button.onclick= () => {
+//     count++;
+//     h1.innerHTML = count;
+// };
+function plus(){
     count++;
-    h1.innerHTML = count;
-};
+    h1.innerHRML = count;
+}
