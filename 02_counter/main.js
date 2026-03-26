12454bcdb5daf4df1fb28a492de1ad77a2bd26f1
yimchaei <babybreadin1025@gmail.com>
2026-03-26 09:31:11 +0900

    counter +, +2, -

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,6 +1,6 @@
 // const h1 = document.querySelector("h1");
 // const button = document.querySelector("button");
-const h1 = document.getElementById("redult")
+const h1 = document.getElementById("result")
 // const plusButton = document.getElementsByClassName("plusButton")[0];
 
 let count = 0;
@@ -15,7 +15,8 @@ let count = 0;
 //     h1.innerHTML = count;
 // };
 h1.innerHTML = count;
-function plus(){
-    count++;
-    h1.innerHRML = count;
+function plus(number=1){
+    count += number;
+    h1.innerHTML = count;
 }
+
