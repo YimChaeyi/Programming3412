570357ecb2ddfc6e3fc14b846732b2033973f073
yimchaei <babybreadin1025@gmail.com>
2026-04-02 09:53:16 +0900

    CounterApp +

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,19 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-// import './App.css'
+// import './App.css';,
 
 function CounterApp() {
+  // count : 상태 값, 이전 상태 값; sercount(): 상태를 바꾸는 함수. 반드시 얘로 바꿔야함
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}
       >
         Count is {count}
-      </button>
+      </button> */}
     </>
   )
 }
