5f11bc1fd92aaeca3d40164712de491e7fae7d43
yimchaei <babybreadin1025@gmail.com>
2026-04-09 09:07:22 +0900

    CounterApp + (최대10까지), 끝 🐖

--- p2/ex6.js
+++ p2/ex6.js
@@ -1,17 +1,29 @@
-const students = [
-  { name: "Kim", scores: [85, 92, 78] },
-  { name: "Lee", scores: [88, 76, 92] },
-  { name: "Park", scores: [90, 95, 80] }
-];
+// const students = [
+//   { name: "Kim", scores: [85, 92, 78] },
+//   { name: "Lee", scores: [88, 76, 92] },
+//   { name: "Park", scores: [90, 95, 80] }
+// ];
 
-const studentsWithAvg = students.map(student => {
-  const sum = student.scores.reduce((acc, cur) => acc + cur, 0);
-  const avg = sum / student.scores.length;
+// const studentsWithAvg = students.map(student => {
+//   const sum = student.scores.reduce((acc, cur) => acc + cur, 0);
+//   const avg = sum / student.scores.length;
 
-  return {
-    name: student.name,
-    average: avg
-  };
-});
+//   return {
+//     name: student.name,
+//     average: avg
+//   };
+// });
 
-console.log(studentsWithAvg);
+// console.log(studentsWithAvg);
+
+// const users = [
+//   { active: true,  profile: { nickname: 'mango' }, scores: [70, 80, 90] },
+//   { active: true,  profile: null,                  scores: [100, 20, 90] },
+//   { active: false, profile: { nickname: 'apple' }, scores: [90, 90, 90] },
+//   { active: true,  profile: {},                    scores: [60, 61, 99] }
+// ];
+
+// const result = users
+// const users = [
+
+// ]
--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,21 +1,35 @@
+// https:///programming3412.babybreadin1025.workers.dev
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-// import './App.css';,
+// import './App.css'
 
 function CounterApp() {
-  // count : 상태 값, 이전 상태 값; sercount(): 상태를 바꾸는 함수. 반드시 얘로 바꿔야함
+  // count : 상태 값, 이전 상태값; setCount(): 상태를 바꾸는 함수. 반드시 얘로 바꿔야함;
   const [count, setCount] = useState(0)
+  function plusMAx10() {{
+    setCount((count) => {
+      if (count >= 10) return 10;
+      return count + 1;
+    })
+  }}
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>+</button>
-      <button onClick={() => setCount((count) => count + 2)}>+2</button>
-      <button onClick={() => setCount((count) => count - 1)}>-</button>
-      <button onClick={() => setCount(0)}>reset</button>
-
+    <h1>{count}</h1>
+    <button onClick={() => setCount((count) => count +1)}>+</button>
+    <button onClick={() => setCount((count) => count +2)}>+2</button>
+    <button onClick={() => setCount((count) => count -1)}>-</button>
+    <button onClick={() => setCount(0)}>reset</button>
+    {/* <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대 10까지)</button> */}
+    <button onClick={() => setCount((count) => count +1 > 10 ? 10 : count + 1)}>+(최대 10까지)</button>
+    <button onClick={() => setCount((count) => {
+      if (count >= 10) return 10;
+      return count + 1;
+    })}>+(최대 10까지)</button>
+    <button onClick={() => plusMAx10()}>+(최대 10까지)</button>
+    <button onClick={plusMAx10}>+(최대 10까지)</button>
 
       {/* <button
         className="counter"
