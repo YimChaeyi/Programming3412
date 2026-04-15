f12cdb5d1d56dd6124d36e85d832606f32ef9078
yimchaei <babybreadin1025@gmail.com>
2026-04-15 11:32:40 +0900

    TodoListApp li -> TodoItemEmpty, button -> Button,input,label -> Checkbox

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,26 +1,27 @@
-import './todolist.css'
+import './todoList.css'
+import TodoItemEmpty from './components/TodoItemEmpty.jsx'
+import Button from './components/Button.jsx';
+import Checkbox from './components/Checkbox.jsx';
 
-function TodoListApp(){
+function TodoListApp() {
     return (
-
         <div className="todo">
             <h1 className="todo__title">HyeonDo List</h1>
-            <form className="todo__form">
-                <input type="text" className="todo__input" placeholder='할 일을 입력하세요'/>
-                <button type="submit" className="todo__button todo__button--add">Add</button>
-            </form>    
-            <ul className="todo__list">
-                <li className="todo__item todo_item--empty">
-                    <p>할일 없음.</p>
-                </li>
 
-                <li className="todo__item todo__item--complete">
-                    <input type="checkbox" id="chk-1" className="todo__check"/>
-                    <label htmlFor="chk-1" className="todo__label">지각 하기</label>
-                    <button className="todo__button todo__button--edit">✏️</button>
-                    <button className="todo__button todo__button--delete">❌</button>
-                </li> 
-            </ul>
+            <form className="todo__form">
+                <input type="text" placeholder="할 일을 입력하세요" className="todo__input"/>
+                <Button type="submit" className="todo__button todo__button--add">Add</Button>
+            </form>
+            <div>
+                <ul className="todo__list">
+                    <TodoItemEmpty/>
+                    <li className="todo__item todo__item--complete">
+                        <Checkbox id="1">지각 하기</Checkbox>
+                        <Button type="button" className="todo__button todo__button--edit">✏️</Button>
+                        <Button type="button" className="todo__button todo__button--delete">❌</Button>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,11 @@
+export default function Checkbox(props){
+    const {children, id} = props
+    return(
+        <>
+            <input type="checkbox" id={`chk-${id}`} className="todo__checkbox"/>
+            <label htmlFor={`chk-${id}`} className="todo__label">{children}
+                <span>지각하기</span>
+            </label>
+        </>
+    )
+}
